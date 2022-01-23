3e7eea8232302913f9c2d9671dafe573ffbe68ed
joethyr <joethayr@gmail.com>
2022-01-23 15:07:48 -0700

    add move directions for queen class.

--- lib/queen.rb
+++ lib/queen.rb
@@ -10,5 +10,19 @@ class Queen
   end
 
   def move_directions
+    [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+      [-1, -1],
+      [-1, 1],
+      [1, 1],
+      [1, -1]
+    ]
   end
 end
+
+# piece moves
+# move diagonally, vertically, or horizontally any number of squares.
+# unable to jump over pieces
