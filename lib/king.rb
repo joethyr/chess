9251bd145fda23ebf1a63adf2dd8ecfe391dbad4
joethyr <joethayr@gmail.com>
2022-01-23 14:26:36 -0700

    add move directions for king class.

--- lib/king.rb
+++ lib/king.rb
@@ -10,5 +10,18 @@ class King
   end
 
   def move_directions
+    [
+      [0,1],
+      [0,-1],
+      [1,0],
+      [-1,0],
+      [-1,-1],
+      [-1,1],
+      [1,1],
+      [1,-1],
+    ]
   end
 end
+
+# pice move
+# one square in any direction, as long as the square is not attacked by an enemy piece
