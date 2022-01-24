4991add7ec3771537a500fb15ed27a40d14b77b0
joethyr <joethayr@gmail.com>
2022-01-24 13:59:03 -0700

    add 'render' method that displays chess symbols on board grid.

--- lib/board_render_text.rb
+++ lib/board_render_text.rb
@@ -1,2 +1,18 @@
+# responsible for rendering the board as text.
 class BoardRenderText
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def render
+    8.times do |row|
+      puts '--------------'
+      8.times do |column|
+        print board[[row, column]]
+      end
+      puts ''
+    end
+  end
 end
