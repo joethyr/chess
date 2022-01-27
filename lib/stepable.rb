fb1b29f38f156516c14be59d8c34a102cef2f542
joethyr <joethayr@gmail.com>
2022-01-26 20:11:35 -0700

    create method 'available_moves'.

--- lib/stepable.rb
+++ lib/stepable.rb
@@ -1,2 +1,17 @@
-module Steppable
+module Stepable
+  def available_moves
+    moves = []
+
+    move_directions.each do |x, y|
+      row, column = coordinate
+
+      row += x
+      column += y
+      coord = [row, column]
+      next unless board.within_bounds?(coord)
+
+      moves << coord if board.empty?(coord) || opponent?(coord)
+    end
+    moves
+  end
 end
