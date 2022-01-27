62ace2d0ab262cf96e0aa38716e3e30366b1ff4e
joethyr <joethayr@gmail.com>
2022-01-26 21:54:48 -0700

    add convient methods for 'current_row' and 'current_column'.

--- lib/piece.rb
+++ lib/piece.rb
@@ -10,4 +10,12 @@ class Piece
   def opponent?(coordinate)
     !board[coordinate].nil? && board[coordinate].color != color
   end
+
+  def current_row
+    coordinate.first
+  end
+
+  def current_column
+    coordinate.last
+  end
 end
