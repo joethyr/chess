12f1e617093c86d255ae6b3a2a5420db4dce9ed6
joethyr <joethayr@gmail.com>
2022-01-27 21:09:39 -0700

    edit code to test play method in Game class.

--- main.rb
+++ main.rb
@@ -2,15 +2,7 @@ require_relative './lib/pieces'
 require_relative './lib/board'
 require_relative './lib/board_render_text'
 require_relative './lib/player'
+require_relative './lib/game'
 
-b = Board.setup_pieces
-display = BoardRenderText.new(b)
-
-b[[2, 2]] = Pawn.new(b, :white, [2, 2])
-b[[2, 0]] = Pawn.new(b, :white, [2, 0])
-p b[[1, 1]].available_moves
-puts display.render
-
-# b = Board.setup_pieces
-# display = BoardRenderText.new(b)
-# puts display.render
+game = Game.new
+game.play
