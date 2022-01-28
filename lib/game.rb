6f4d178ed0cc27b5f9215d1c48bfae78ecd95d07
joethyr <joethayr@gmail.com>
2022-01-27 19:55:15 -0700

    add initialize method set players and the current player.

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,15 @@
 class Game
+  attr_reader :player1, :player2
+
+  def initialize
+    @player1 = Player.new(:black)
+    @player2 = Player.new(:white)
+    @current_player = @player1
+  end
+
+  def play
+    # prompt player to enter a starting position
+    # prompt player to enter a ending position
+    # move the piece
+  end
 end
