fa0a52cc39fdf6ea438081ff41efdf9d8e5825c3
joethyr <joethayr@gmail.com>
2022-01-27 19:49:49 -0700

    update instance variable 'coordinate' to be a getter and setter.

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,5 +1,7 @@
 class Piece
-  attr_reader :board, :color, :coordinate
+  attr_reader :board, :color
+
+  attr_accessor :coordinate
 
   def initialize(board, color, coordinate)
     @board = board
@@ -19,3 +21,23 @@ class Piece
     coordinate.last
   end
 end
+
+  def move_piece(start_position, end_position)
+    # validate that 'end_position is in available moves'
+    piece = self[start_position]
+    unless piece.available_moves.include?(end_position)
+      raise "End position #{end_position} is not an available move."
+    end
+
+    # validate that 'end_position is within board bounds.'
+    raise 'End position not within board bounds.' unless within_bounds?(end_position)
+
+    # remove piece from the board at the start position
+    self[start_position] = nil
+
+    # place the piece on the board at the new location
+    self[end_position] = piece
+
+    # update the piece's internal location with end position
+    piece.coordinate = end_position
+  end
