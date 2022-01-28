1472c6486ebcbc46d6539e6598ced43193c82a70
joethyr <joethayr@gmail.com>
2022-01-27 19:56:12 -0700

    add method that switches the current player.

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,15 @@
 class Game
   attr_reader :player1, :player2
+  attr_accessor :current_player
 
   def initialize
     @player1 = Player.new(:black)
     @player2 = Player.new(:white)
     @current_player = @player1
+  end
+
+  def switch_current_player
+    self.current_player = (current_player == player1 ? player2 : player1)
   end
 
   def play
