0eb8e01ff84346fe748aaf390e00cfd8f2557a6b
joethyr <joethayr@gmail.com>
2022-01-28 21:29:06 -0700

    add board renderer into class.

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,13 @@
 class Game
-  attr_reader :player1, :player2, :board
+  attr_reader :player1, :player2, :board, :renderer
   attr_accessor :current_player
 
-  def initialize(player1, player2, board)
+  def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @board = board
+    @board = Board.setup_pieces
     @current_player = @player1
-
+    @renderer = BoardRender.new(board)
   end
 
   def switch_current_player
@@ -15,10 +15,17 @@ class Game
   end
 
   def play
+    renderer.render
     puts "It's color #{current_player.color}'s turn."
-    puts "Enter a piece to move:"
-    start_position = get_position
-    p start_position
+    loop do
+      puts "Enter a piece to move:"
+      start_position = get_position
+      if board[start_position].color == current_player.color
+        break
+      end
+      puts "did not select a #{current_player.color} piece."
+      p start_position
+    end
     puts "Enter a position to move to:"
     end_position = get_position
     p end_position
