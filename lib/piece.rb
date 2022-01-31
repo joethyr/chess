d99f21b4f2bd499ea59028ef84500d5173ee37be
joethyr <joethayr@gmail.com>
2022-01-30 19:59:24 -0700

    add method that checks the available moves that dont move to check.

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,6 +1,5 @@
 class Piece
   attr_reader :board, :color
-
   attr_accessor :coordinate
 
   def initialize(board, color, coordinate)
@@ -20,24 +19,16 @@ class Piece
   def current_column
     coordinate.last
   end
-end
 
-  def move_piece(start_position, end_position)
-    # validate that 'end_position is in available moves'
-    piece = self[start_position]
-    unless piece.available_moves.include?(end_position)
-      raise "End position #{end_position} is not an available move."
+
+  def safe_moves
+    moves = []
+    available_moves.each do |move|
+      new_board = board.duplicate
+      new_board.move_piece(coordinate, move)
+      if !new_board.check?(color)
+        moves << move
     end
-
-    # validate that 'end_position is within board bounds.'
-    raise 'End position not within board bounds.' unless within_bounds?(end_position)
-
-    # remove piece from the board at the start position
-    self[start_position] = nil
-
-    # place the piece on the board at the new location
-    self[end_position] = piece
-
-    # update the piece's internal location with end position
-    piece.coordinate = end_position
+    moves
   end
+end
