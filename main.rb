b64b6cca8fb11c99710c40ecafc63f45c56cc16c
joethyr <joethayr@gmail.com>
2022-01-30 22:25:04 -0700

    move all chess pieces into their own folder.

--- main.rb
+++ main.rb
@@ -3,8 +3,17 @@ require_relative './lib/board'
 require_relative './lib/board_render'
 require_relative './lib/player'
 require_relative './lib/game'
+require_relative './lib/invalid_move_error'
+
+
+b = Board.new
+b[[0, 0]] = King.new(b, :black, [0, 0])
+b[[6, 6]] = King.new(b, :white, [6, 6])
+
+b[[3, 0]] = Rook.new(b, :white, [3, 0])
+b[[3, 1]] = Rook.new(b, :white, [3, 1])
+
+# puts b.checkmate?(:white)
 
 game = Game.new(Player.new(:black), Player.new(:white))
 game.play
-# renderer = BoardRender.new(board)
-# renderer.render
